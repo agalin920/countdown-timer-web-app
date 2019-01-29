e4a530d6085fd36a2020a0e50ca17968f6482017
agalin920 <agalin920@gmail.com>
2019-01-28 21:32:03 -0600

    Updates

---
+++ precache-manifest.b7747d718838305ab65700ed56be9635.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "de3647c1d32513040111",
+    "url": "/countdown-timer-web-app/static/js/runtime~main.de3647c1.js"
+  },
+  {
+    "revision": "9ffb0eda94351b9727cd",
+    "url": "/countdown-timer-web-app/static/js/main.9ffb0eda.chunk.js"
+  },
+  {
+    "revision": "7f50a2bc032c0b4d9cf1",
+    "url": "/countdown-timer-web-app/static/js/1.7f50a2bc.chunk.js"
+  },
+  {
+    "revision": "9ffb0eda94351b9727cd",
+    "url": "/countdown-timer-web-app/static/css/main.13ac906b.chunk.css"
+  },
+  {
+    "revision": "469f643526f8d511268594274920a5ac",
+    "url": "/countdown-timer-web-app/index.html"
+  }
+];
